0c70f829578855a12bd6b38782344832b764414e
DenisKP <denis.buserski@protonmail.com>
2024-11-23 14:25:02 +0200

    Update

--- src/main/java/com/puppiespassion/web/HomeController.java
+++ src/main/java/com/puppiespassion/web/HomeController.java
@@ -9,31 +9,31 @@ public class HomeController {
 
     @GetMapping("/")
     public String homeNoPath() {
-        return "home.html";
+        return "home";
     }
 
     @GetMapping("/home")
     public String home() {
-        return "home.html";
+        return "home";
     }
 
     @GetMapping("/about")
     public String aboutUs() {
-        return "static-pages/about.html";
+        return "static-pages/about";
     }
 
     @GetMapping("/contacts")
     public String contactUs() {
-        return "static-pages/contacts.html";
+        return "static-pages/contacts";
     }
 
     @GetMapping("/faq")
     public String faq() {
-        return "static-pages/faq.html";
+        return "static-pages/faq";
     }
 
     @GetMapping("/refund")
     public String refund() {
-        return "static-pages/refund.html";
+        return "static-pages/refund";
     }
 }
--- src/main/java/com/puppiespassion/web/ProductController.java
+++ src/main/java/com/puppiespassion/web/ProductController.java
@@ -1,4 +1,32 @@
 package com.puppiespassion.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
 public class ProductController {
+
+    private static final List<String> CATEGORIES = List.of("bags", "beds", "bowls", "clothes", "combs", "jewelry", "leashes", "toys");
+    @GetMapping("/products")
+    public String getAllProductsPage() {
+        return "products/products.html";
+    }
+
+    @GetMapping("/products/category/{category}")
+    public String getCategoryPage(@PathVariable String category) {
+
+        if (CATEGORIES.contains(category)) {
+            return "products/" + category;
+        }
+        return "redirect:/products";
+    }
 }
+
+
+
+
+
+
